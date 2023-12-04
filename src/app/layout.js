33254ad5d7ec2cf92d423b8dd9413ce35b30ea67
SamarthParmanand <parmanandsamarth@gmail.com>
2023-12-04 22:47:41 +0530

    feat: nav-bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,14 @@
 import { Inter } from 'next/font/google'
+import { Josefin_Sans } from 'next/font/google'
 import './globals.css'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
+const josefin = Josefin_Sans({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={josefin.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
