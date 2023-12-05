92b1ea0e4610e624dddeeee14b2afc5ad270b2c5
SamarthParmanand <parmanandsamarth@gmail.com>
2023-12-05 14:30:16 +0530

    feat: hero gsap

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,27 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect } from "react";
 import classes from "./Hero.module.css";
+import gsap from "gsap";
 
 const Hero = () => {
+
+  useEffect(() => {
+    gsap.fromTo('#hero-section', {opacity: 0, scale: 0.75, y: -10}, {opacity: 1, scale: 1, y: 0, duration: 0.75});
+    gsap.fromTo('#where', {x: -100}, {x: 0, duration: 1});
+    gsap.fromTo('#design', {x: -100}, {x: 0, duration: 1});
+    gsap.fromTo('#becomes', {x: -100}, {x: 0, duration: 1});
+    gsap.fromTo('#destiny', {x: -100}, {x: 0, duration: 1});
+  }, [])
+  
+
   return (
     <>
-      <div className={`${classes.heroBG} flex flex-col lg:flex-row md:flex-row justify-center items-center relative text-white  `}>
-        <span className={`font-seasons py-4 lg:px-4 md:px-4 text-4xl lg:text-7xl md:text-5xl relative z-10`}>Where</span>
-        <span className={`font-seasonsItalics lg:pr-2 md:pr-2 text-5xl lg:text-8xl md:text-6xl relative z-10`}>Design</span>
-        <span className={`font-seasons py-4 lg:px-4 md:px-4 text-4xl lg:text-7xl md:text-5xl relative z-10`}>Becomes</span>
-        <span className={`font-seasonsItalics lg:pr-2 md:pr-2 text-5xl lg:text-8xl md:text-6xl relative z-10`}>Destiny</span>
+      <div id="hero-section" className={`${classes.heroBG} flex flex-col lg:flex-row md:flex-row justify-center items-center relative text-white `}>
+        <span id="where" className={`font-seasons py-4 lg:px-4 md:px-4 text-4xl lg:text-7xl md:text-5xl relative z-10 `}>Where</span>
+        <span id="design" className={`font-seasonsItalics lg:pr-2 md:pr-2 text-5xl lg:text-8xl md:text-6xl relative z-10 `}>Design</span>
+        <span id="becomes" className={`font-seasons py-4 lg:px-4 md:px-4 text-4xl lg:text-7xl md:text-5xl relative z-10 `}>Becomes</span>
+        <span id="destiny" className={`font-seasonsItalics lg:pr-2 md:pr-2 text-5xl lg:text-8xl md:text-6xl relative z-10 `}>Destiny</span>
       </div>
     </>
   );
