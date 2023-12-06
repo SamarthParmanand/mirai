61c84788ad146ec247da6e229d0c13183dbb8b21
SamarthParmanand <parmanandsamarth@gmail.com>
2023-12-06 19:51:24 +0530

    fix: lenis-scroll

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,20 @@ import Hero from "@/pages/Hero/Hero";
 import About from "@/pages/About/About";
 import Projects from "@/pages/Projects/Projects";
 import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 export default function Home() {
-  const lenis = new Lenis();
 
-  lenis.on("scroll", (e) => {
-    console.log(e);
-  });
-
-  function raf(time) {
-    lenis.raf(time);
+  useEffect(() => {
+    const lenis = new Lenis();
+  
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+  
     requestAnimationFrame(raf);
-  }
-
-  requestAnimationFrame(raf);
+  })
 
   return (
     <>
