97525786c270b3e1188c25277a3c67871bffc561
SamarthParmanand <parmanandsamarth@gmail.com>
2023-12-07 12:29:45 +0530

    fix: projects + mobile view

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useLayoutEffect } from "react";
+import React, { useRef } from "react";
 import {
   floater1,
   floater2,
@@ -20,104 +20,100 @@ const Projects = () => {
   const plane2 = useRef(null);
   const plane3 = useRef(null);
 
-  useLayoutEffect(() => {
-    const handleMouseMove = (e) => {
-      const { movementX, movementY } = e;
-      console.log(movementX, movementY);
+  const handleMouseMove = (e) => {
+    const { movementX, movementY } = e;
 
-      gsap.to(plane1.current, {
-        x: `+=${movementX * 0.01}`,
-        y: `+=${movementY * 0.01}`,
-      });
-
-      gsap.to(plane2.current, {
-        x: `+=${movementX * 0.005}`,
-        y: `+=${movementY * 0.005}`,
-      });
-
-      gsap.to(plane3.current, {
-        x: `+=${movementX * 0.0025}`,
-        y: `+=${movementY * 0.0025}`,
-      });
-    };
-    const projects = document.querySelector("#projects");
-    console.log(projects);
-    projects.addEventListener("mousemove", handleMouseMove);
-    return () => {
-      projects.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
+    gsap.set(plane1.current, {
+      x: `+=${movementX * 0.015}`,
+      y: `+=${movementY * 0.015}`,
+      ease: 'power4.in'
+    });
+    
+    gsap.set(plane2.current, {
+      x: `+=${movementX * 0.012}`,
+      y: `+=${movementY * 0.012}`,
+      ease: 'power4.in'
+    });
+    
+    gsap.set(plane3.current, {
+      x: `+=${movementX * 0.02}`,
+      y: `+=${movementY * 0.02}`,
+      ease: 'power4.in'
+    });
+  };
 
   return (
     <div
-      className="relative h-[125vh] my-[10%] w-[100vw] z-1"
-      // onMouseMove={(e) => handleMouseMove(e)}
+      className="relative h-[90vh] lg:h-[125vh] my-[5%] w-[100vw] z-1 overflow-hidden"
+      onMouseMove={(e) => handleMouseMove(e)}
       id="projects"
     >
-      <div className="w-[100%] h-[100%] z-0 absolute " id="plane1" ref={plane1}>
+      <div
+        className="w-[100%] h-[100%] z-0 absolute pointer-events-none"
+        id="plane1"
+        ref={plane1}
+      >
         <Image
           src={floater1}
           alt="floater1"
-          width={285}
-          className="absolute brightness-50 left-[90%] top-[70%]"
+          className="absolute brightness-50 left-[60%] lg:left-[80%] top-[60%] lg:top-[50%] w-[95px] lg:w-[285px]"
         />
         <Image
           src={floater2}
           alt="floater2"
-          width={300}
-          className="absolute brightness-50 left-[5%] top-[45%]"
+          className="absolute brightness-50 left-2 lg:left-[12%] top-[54%] lg:top-[45%] w-[100px] lg:w-[300px] "
         />
         <Image
           src={floater3}
           alt="floater3"
-          width={300}
-          className="absolute brightness-50 left-[80%] top-[15%]"
+          className="absolute brightness-50 left-[40%] lg:left-[80%] top-[25%] lg:top-[15%] w-[100px] lg:w-[300px]"
         />
         <Image
           src={floater4}
           alt="floater4"
-          width={275}
-          className="absolute brightness-50 left-[25%] top-[0%] "
+          className="absolute brightness-50 left-[7%] lg:left-[30%] top-[28%] lg:top-[0%] w-[85px] lg:w-[275px]"
         />
       </div>
-      <div className="w-[100%] h-[100%] z-0 absolute " id="plane2" ref={plane2}>
+      <div
+        className="w-[100%] h-[100%] z-0 absolute pointer-events-none"
+        id="plane2"
+        ref={plane2}
+      >
         <Image
           src={floater5}
           alt="floater5"
-          width={325}
-          className="absolute brightness-40 left-[10%] top-[80%]"
+          className="absolute brightness-40 -left-5 lg:left-[25%] top-[60%] lg:top-[55%] w-[110px] lg:w-[325px]"
         />
         <Image
           src={floater6}
           alt="floater6"
-          width={280}
-          className="absolute brightness-40 left-[50%] top-[7%]"
+          className="absolute brightness-40 left-[50%] top-[26%] lg:top-[7%] w-[100px] lg:w-[280px]"
         />
         <Image
           src={floater7}
           alt="floater7"
-          width={300}
-          className="absolute brightness-40 left-[35%] top-[60%]"
+          className="absolute brightness-40 left-[19%] lg:left-[45%] top-[67%] lg:top-[50%] w-[100px] lg:w-[300px]"
         />
       </div>
-      <div className="w-[100%] h-[100%] z-0 absolute " id="plane3" ref={plane3}>
+      <div
+        className="w-[100%] h-[100%] z-0 absolute pointer-events-none"
+        id="plane3"
+        ref={plane3}
+      >
         <Image
           src={floater8}
           alt="floater8"
-          width={285}
-          className="absolute brightness-40 left-[65%] top-[70%]"
+          className="absolute brightness-40 left-[0%] lg:left-[65%] top-[52%] lg:top-[70%] w-[95px] lg:w-[285px]"
         />
         <Image
           src={floater9}
           alt="floater9"
-          width={300}
-          className="absolute brightness-40 left-[70%] top-[35%]"
+          className="absolute brightness-40 -left-[7%] lg:left-[70%] top-[10%] lg:top-[35%] w-[100px] lg:w-[300px]"
         />
         <Image
           src={floater10}
           alt="floater10"
-          width={275}
-          className="absolute brightness-40 left-0 top-[10%]"
+          className="absolute brightness-40 left-[40%] lg:left-[15%] top-[11%] lg:top-[10%] w-[90px] lg:w-[275px]"
         />
       </div>
       <div className="absolute top-[45%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex flex-col justify-center items-center">
